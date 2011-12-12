c0332f27675413d235b68ba8e9e77553a50a51e5
cbollmann@stud.hs-bremen.de <cbollmann@stud.hs-bremen.de@4afa6320-88a6-697d-641d-d9d6f956909d>
2011-12-12 05:21:19 +0000

    Initial commit.

---
+++ trunk/src/edu/fhooe/mtd360/watershader/App.java
@@ -0,0 +1,23 @@
+package edu.fhooe.mtd360.watershader;
+
+import java.io.IOException;
+
+/**
+ * TODO: Initial app, just change to actual content
+ * 
+ * @author TAKERU
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		System.out.println("This is an app!");
+		System.out.println("Press any key to continue...");
+		System.in.read();
+	}
+
+}
